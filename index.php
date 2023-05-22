ab2997f7ac7e35648cfefa8f10068a7dfba8ac1e
garreaudv <garreaudv@uc.cl>
2023-05-22 15:43:10 -0400

    actualizacion tablas

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <title>Consultas</title>
   <head>
-  <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="<link rel="stylesheet" href="/shared/css/5.0/css/stylesheet.css" type="text/css">">
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
   <script>
